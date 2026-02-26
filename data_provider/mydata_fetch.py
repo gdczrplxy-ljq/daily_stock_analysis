26880f4595ed68950bd6830aa32f228db6ca4044
gdczrplxy-ljq <gdczrplxy@gmail.com>
2026-02-26 14:42:04 +0800

    Update data_provider/mydata_fetch.py with new content

--- data_provider/mydata_fetch.py
+++ data_provider/mydata_fetch.py
@@ -4,12 +4,15 @@ import json
 from cachetools import TTLCache, cached
 
 class MairuiapiDataFetcher:
-    def __init__(self, cache_size=100, ttl=300):
+    def __init__(self, cache_size=100, ttl=300, api_key='670F47A4-C18B-47CC-A4E1-9C5178B69603'):
         self.cache = TTLCache(maxsize=cache_size, ttl=ttl)
+        self.api_key = api_key
+        self.base_url = 'https://api.mairuiapi.com/hsstock/latest'
 
-    @cached(cache)
+    @cached(cache={})
     def fetch_stock_data(self, stock_code):
-        api_url = f'https://api.mairuiapi.com/hsstock/latest/?symbol={stock_code}'
+        # 构建API URL: https://api.mairuiapi.com/hsstock/latest/{stock_code}/d/n/{api_key}
+        api_url = f'{self.base_url}/{stock_code}/d/n/{self.api_key}'
         try:
             response = requests.get(api_url)
             response.raise_for_status()  # Raise an error for bad responses
@@ -29,5 +32,5 @@ class MairuiapiDataFetcher:
 
 # Example usage:
 # fetcher = MairuiapiDataFetcher()
-# stock_data = fetcher.get_real_time_quote('AAPL')
-# print(stock_data) \n
+# stock_data = fetcher.get_real_time_quote('000001.SZ')
+# print(stock_data)
